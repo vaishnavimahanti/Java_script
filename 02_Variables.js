e92b9d802596a937766c5ff657c1b2ecd1da9cfd
Mahanti Vaishnavi <108821621+vaishnavimahanti@users.noreply.github.com>
2025-03-28 19:44:01 +0000

    container config done

---
+++ 02_Variables.js
@@ -0,0 +1,14 @@
+const a = 5 // can't be changed in anyway scope
+let b = 4 //declaration of the variable
+var c = 3
+function add(a,b,c){
+    b = 2 //updating the variable 
+    var c = 1 //function scoped so the actual value of c outside the function doesn't change
+    console.log(b)
+    console.log(c)
+}
+
+var c = 0 // global scoped the actual value of the c is changed from 3 to 0
+add()
+console.log(b)
+console.log(c)
