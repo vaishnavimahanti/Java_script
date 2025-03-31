f7accc6438c5d79104584345b06115c5680fc906
Mahanti Vaishnavi <108821621+vaishnavimahanti@users.noreply.github.com>
2025-03-31 18:14:46 +0000

    container config done

--- Pratice_set2.js
+++ Pratice_set2.js
@@ -1,7 +1,45 @@
-let age = 15
+let age = 21
+/*
 if(age >= 10 && age<=20){
     console.log(true);
 }
 else{
     console.log(false);
 }
+
+*/
+
+/*
+switch(age){
+    case 11: 
+    console.log('This is 11');
+    break;
+    case 12: 
+    console.log('This is 12');
+    break;
+    case 13: 
+    console.log('This is 13');
+    break;
+    default: 
+    console.log('This number is out of range')
+}
+
+*/
+
+/*
+if(age%2==0 && age%3==0){
+    console.log('Number divisble by 2 and 3');
+}
+else if(age%2==0){
+    console.log('Number divisble by 2');
+}
+else if (age%3==0){
+    console.log('Number divisible by 3');
+}
+else{
+    console.log('not divisble by 2 and 3');
+}
+*/
+
+console.log(age>=18? "You can drive": "You can't drive");
+
